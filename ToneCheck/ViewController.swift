2ef77e7a8a78eb8ac3981abfd545dd4a0a43193c
Hideyasu Ishii <ishii_pma@yahoo.co.jp>
2025-03-19 21:45:14 +0900

    Initial Commit

---
+++ ToneCheck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToneCheck
+//
+//  Created by HIDEYASU ISHII on 2025/03/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
